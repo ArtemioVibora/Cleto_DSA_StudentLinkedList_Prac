e51967320c29f94d09e6a18854ec559c8aa2eadd
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-09 21:22:01 +0800

    Brother give me burger

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <malloc.h>
+//Single LinkedList
 
+struct STUDENT {
+    char name[100];
+    char surname[100];
+    int age;
+    int studentID;
+    STUDENT *next;
+};
