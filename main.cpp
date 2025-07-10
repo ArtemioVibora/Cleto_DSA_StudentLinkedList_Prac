c3e472f267e6ac4fb35582373666de78c39d8c66
nina <juan.amado.sicat.cleto@gmail.com>
2025-07-10 20:55:42 +0800

    Brother give me burger

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <malloc.h>
+#include <cstring>
 //Single LinkedList
 
 struct STUDENT {
     char name[100];
     char surname[100];
+    char address[100];
     int age;
     int studentID;
     STUDENT *next;
 };
+
+void addNewStudent(STUDENT **student, char n[], char sn[], char a[], int age, int studentID) {
+    STUDENT *newStudent, *ptr2;
+    newStudent = (STUDENT *)malloc(sizeof(STUDENT));
+    strcpy(newStudent->name, n);
+    strcpy(newStudent->surname, sn);
+    strcpy(newStudent->address, a);
+    newStudent->age = age;
+    newStudent->studentID = studentID;
+}
+
+int main() {
+
+}
